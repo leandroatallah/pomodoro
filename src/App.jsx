b41bea2ae37fd4e3449e2a807de882bf560cc89a
leandro atallah <leandroatallah@gmail.com>
2020-02-21 16:48:14 -0300

    Tema gradiente

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import {Container, Row, Col, Button, Badge, Jumbotron, Card} from 'react-bootstrap';
+import {Container, Row, Col, Button, Badge, Jumbotron} from 'react-bootstrap';
+import './assets/main.css'
 import notificationMp3 from './assets/notification.mp3'
 import notificationOgg from './assets/notification.ogg'
 
@@ -35,7 +36,7 @@ export default function App() {
             if(!paused && count) {
                 setCount(count => count ? count - 1 : 0)
             }
-        }, 0)
+        }, 1000)
         return () => clearInterval(interval)
     }, [count, working, breakCount, paused, sessionTime, breakTime])
 
@@ -141,9 +142,8 @@ export default function App() {
                         <h2><Badge variant={working ? "success" : "primary"}>{working ? 'Working' : (breakCount >= 3 ? 'Long Break' : 'Break')}</Badge></h2>
                     </Col>
                 </Row>
-                <hr/>
                 <br/>
-                <Jumbotron fluid>
+                <Jumbotron>
                 <h1 className="text-center">{timerConvert(count)}</h1>
                 </Jumbotron>
                 <Row>
@@ -151,35 +151,35 @@ export default function App() {
                         <Button variant="success" size="lg" block onClick={() => startTimer()} disabled={!paused}>Start</Button>
                     </Col>
                     <Col>
-                        <Button variant="secondary" size="lg" block onClick={() => setPaused(true)} disabled={paused}>Stop</Button>
+                        <Button variant="light" size="lg" block onClick={() => setPaused(true)} disabled={paused}>Stop</Button>
                     </Col>
                     <Col>
-                        <Button variant="secondary" size="lg" block onClick={() => resetTimer()} disabled={paused}>Reset</Button>
+                        <Button variant="light" size="lg" block onClick={() => resetTimer()}>Reset</Button>
                     </Col>
                 </Row>
 
                 <br/>
 
-                <Card style={{padding: '20px'}}>
+                <Jumbotron style={{padding: '20px'}}>
                     <Row>
                         <Col className="text-center">
                             <h5>Session</h5>
                             <Row className="justify-content-center">
-                                <Button variant="info" onClick={() => updateSessionTime(sessionTime + 60)}>+</Button>
+                                <Button variant="light" onClick={() => updateSessionTime(sessionTime + 60)}>+</Button>
                                 <h4 style={{padding: '0 20px'}}>{sessionTime / 60}</h4>
-                                <Button variant="info" onClick={() => updateSessionTime(sessionTime - 60)}>-</Button>
+                                <Button variant="light" onClick={() => updateSessionTime(sessionTime - 60)}>-</Button>
                             </Row>
                         </Col>
                         <Col className="text-center">
                             <h5>Break</h5>
                             <Row className="justify-content-center">
-                                <Button variant="info" onClick={() => updateBreakTime(breakTime + 60)}>+</Button>
+                                <Button variant="light" onClick={() => updateBreakTime(breakTime + 60)}>+</Button>
                                 <h4 style={{padding: '0 20px'}}>{breakTime / 60}</h4>
-                                <Button variant="info" onClick={() => updateBreakTime(breakTime - 60)}>-</Button>
+                                <Button variant="light" onClick={() => updateBreakTime(breakTime - 60)}>-</Button>
                             </Row>
                         </Col>
                     </Row>
-                </Card>
+                </Jumbotron>
             </Container>
         </>
     )
